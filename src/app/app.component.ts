bcfbf2039026e1619397029f7159d79b3dce549d
Tumay Yildiz <tumay.yildiz@loomis.com>
2023-09-28 12:02:54 +0200

    V1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'APipes';
+
+  name = '';
+  date = '';
+  prix: number = 0;
+  DureeMission: number = 0;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onPrixChange(value: string) {
+    this.prix = parseFloat(value);
+  }
+
+  onDureeMissionChange(value: string) {
+    this.DureeMission = parseInt(value) * this.prix;
+  }
+
+
 }
